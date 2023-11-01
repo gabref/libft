ba81e12f5dc726de23df0c6a2acbc6b8e0ea4c50
gabref <gabrielfranzer@gmail.com>
2023-11-01 22:46:48 +0100

    add ft_isascii

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: galves-f <galves-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 20:43:08 by galves-f          #+#    #+#             */
-/*   Updated: 2023/11/01 21:52:35 by galves-f         ###   ########.fr       */
+/*   Updated: 2023/11/01 22:44:53 by galves-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 #endif
