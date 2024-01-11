ea8df2bc0ac5627a4dc4b9a8145891e6801d2fa7
gabref <gabrielfranzer@gmail.com>
2024-01-11 16:09:23 +0100

    feat: formatting

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: galves-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: galves-f <galves-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 00:17:45 by galves-f          #+#    #+#             */
-/*   Updated: 2024/01/10 20:08:19 by galves-f         ###   ########.fr       */
+/*   Updated: 2024/01/11 15:56:44 by galves-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*newstr;
-	int				size_substring;
-	int	original_size;
+	char	*newstr;
+	int		size_substring;
+	int		original_size;
 
 	if (s == NULL)
 		return (NULL);
 	original_size = ft_strlen(s);
-	if (start >= (unsigned int) original_size)
+	if (start >= (unsigned int)original_size)
 		return (ft_strdup(""));
-	size_substring = original_size - start;;
-	if (len > (unsigned int) size_substring)
+	size_substring = original_size - start;
+	if (len > (unsigned int)size_substring)
 		len = size_substring;
 	newstr = (char *)malloc(sizeof(char) * len + 1);
 	if (newstr == NULL)
