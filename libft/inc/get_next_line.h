da8e1727f74a242003feb57d1bdf2b8fe6033111
gabref <gabrielfranzer@gmail.com>
2024-02-12 10:05:47 +0100

    refactor: organizing files, good makefile and add gnl

---
+++ libft/inc/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galves-f <galves-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 18:18:18 by galves-f          #+#    #+#             */
+/*   Updated: 2024/02/12 10:04:20 by galves-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int		has_newline(int start, char *str);
+int		find_newline(char *str);
+int		ft_strlen(char *str);
+char	*strjoin(char *s1, char *s2, int s1_len, int s2_len);
+
+#endif
